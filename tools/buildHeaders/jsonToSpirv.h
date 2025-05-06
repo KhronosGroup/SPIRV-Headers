ce3dfa383e1da71658a1243c9d14d9ebcddad44f
Victor Lomuller <victor@codeplay.com>
2025-05-06 12:31:48 +0100

    Remove asciidoc markup in JSON annotations
    The generator is now handling them

--- tools/buildHeaders/jsonToSpirv.h
+++ tools/buildHeaders/jsonToSpirv.h
@@ -147,6 +147,7 @@ public:
     void setOptional();
     OperandClass getClass(int op) const { return opClass[op]; }
     const char* getDesc(int op) const { return desc[op].c_str(); }
+    void setDesc(int op, const std::string& d) { desc[op] = d; }
     bool isOptional(int op) const { return optional[op]; }
     int getNum() const { return (int)opClass.size(); }
 
